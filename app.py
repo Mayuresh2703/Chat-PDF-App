d03dfe95894dca5518ed1ed2a24d9eba7b241412
Mayuresh Shinge <103566665+Mayuresh2703@users.noreply.github.com>
2023-11-01 00:51:00 +0530

    Update app.py
    Added more functionalities which includes Multi-turn Conversation, Context Preservation and defining Conversation Boundaries.

--- app.py
+++ app.py
@@ -13,23 +13,36 @@ import os
 
 os.environ["OPENAI_API_KEY"] = "YOUR_OPEN_AI_API_KEY"
 
-#sidebar contents
+class ConversationBuffer:
+    def __init__(self):
+        self.buffer = []
 
+    def add_message(self, message):
+        self.buffer.append(message)
+
+    def get_messages(self):
+        return self.buffer
+
+    def clear_buffer(self):
+        self.buffer = []
+
+# Initialize a conversation buffer
+conversation_buffer = ConversationBuffer()
+
+# Sidebar contents
 with st.sidebar:
     st.title('LLM Chat App')
     st.markdown('''
     ## About
-    This app is an LLM powered chatbot  using:
+    This app is an LLM powered chatbot using:
     - [Streamlit](https://streamlit.io/)
     - [LangChain](https://python.langchain.com/)
     - [OpenAI](https://platform.openai.com/docs/models) LLM model                
                 
-                
     ''')
     add_vertical_space(5)
-    st.write("Made by [Mayuresh] (https://github.com/Mayuresh2703)")  
+    st.write("Made by [Mayuresh] (https://github.com/Mayuresh2703)")
 
-    
 def main():
     st.header("Chat with PDF")
 
@@ -65,23 +78,33 @@ def main():
             with open(f"{store_name}.pk1", "wb") as f:
                 pickle.dump(VectorStore, f)
 
-        # Accept user questions/query
-        query = st.text_input("Ask questions about your PDF file:")
-        st.write(query)
+    # Accept user questions/query
+    query = st.text_input("Ask questions about your PDF file:")
+    st.write(query)
 
-        if query:
-            docs = VectorStore.similarity_search(query=query)
+    if query:
+        docs = VectorStore.similarity_search(query=query)
 
-            # Create an instance of OpenAI
-            llm = OpenAI()
+        # Create an instance of OpenAI
+        llm = OpenAI()
 
-            # Specify the chain type without an extra field
-            chain = load_qa_chain(llm=llm)
+        # Specify the chain type without an extra field
+        chain = load_qa_chain(llm=llm)
 
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
+        with get_openai_callback() as cb:
+            response = chain.run(input_documents=docs, question=query)
+            conversation_buffer.add_message(("User", query))
+            conversation_buffer.add_message(("Chatbot", response))
+
+        st.write(response)
+
+    # Display conversation history
+    conversation_history = conversation_buffer.get_messages()
+    for role, message in conversation_history:
+        if role == "User":
+            st.text_input("User:", message, key=message)
+        elif role == "Chatbot":
+            st.text_input("Chatbot:", message, key=message)
 
 if __name__ == '__main__':
     main()
